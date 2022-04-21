a424504012e52cd78a0de4d9168cd7a654f0adfc
Rana Abdul Muneem <r.abdulmuneem@gmail.com>
2022-04-21 14:07:51 +0500

    Created job page

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,9 +3,9 @@ import {Container, Navbar, Nav} from "react-bootstrap"
 const NavBar = () => {
     return  <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="#home"><i>Studento</i></Navbar.Brand>
+    <Navbar.Brand href="/"><i>Studento</i></Navbar.Brand>
     <Nav className="">
-      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="/">Home</Nav.Link>
       <Nav.Link href="#features">Sign up</Nav.Link>
       <Nav.Link href="#pricing">Log in</Nav.Link>
     </Nav>
