e0bf79ec8f0b77fa6311aea4a0b5c4194b43b6e2
Behzad Khokher <behzadkhokher@gmail.com>
2022-04-22 02:53:29 +0500

    commit-company-student-loginandsignup

--- src/components/StudentSignup/LeftSide.js
+++ src/components/StudentSignup/LeftSide.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
 import "./StudentSignup.css";
-import SignUpForm from "./SignUpForm";
+import SignUpForm2 from "./SignUpForm2";
 
 const LeftSide = () => {
-  return <SignUpForm></SignUpForm>;
+  return <div>Left Side there</div>;
 };
 
 export default LeftSide;
