8ef01f60227b1791ef938b6dc78e8cb4499cec36
Rana Abdul Muneem <r.abdulmuneem@gmail.com>
2022-04-22 20:04:23 +0500

    Fixed filter checkboxes

--- src/components/jobs/JobFilter.js
+++ src/components/jobs/JobFilter.js
@@ -1,6 +1,6 @@
 import './JobFilter.css'
 
-import {Component} from 'react'
+import {Component, useState} from 'react'
 import {Accordion} from 'react-bootstrap'
 import {AiOutlinePlusCircle} from 'react-icons/ai'
 
@@ -37,67 +37,95 @@ class Salary extends Component {
     }
 }
 
-class DynamicCheckControls extends Component {
+class CheckControls extends Component {
     constructor(props){
         super(props)
-        this.state = {
-            new_check: '',
-            checks: props.checks
+        let init_checkboxes = [];
+        for (let i=0; i < Math.min(props.values.length, 5); i++){
+            init_checkboxes.push(
+                {value: props.values[i], checked: false}
+            )
         }
-        this.handleChange = this.handleChange.bind(this)
+        console.log("initial", init_checkboxes)
+        this.state = {
+            dynamic: props.dynamic,
+            new_check: '',
+            checkboxes: init_checkboxes
+        }
+        
+        
+        this.handleType = this.handleType.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    handleChange(event) {
+    handleType(event) {
         this.setState({new_check: event.target.value})
     }
 
     handleSubmit(event) {
+        let updatedCheckboxes = this.state.checkboxes
+        updatedCheckboxes.unshift({value: this.state.new_check, checked: true})
+        updatedCheckboxes = updatedCheckboxes.slice(0, 5)
         this.setState({
-            checks:[ this.state.new_check, ... this.state.checks],
+            checkboxes: updatedCheckboxes,
             new_check: ''
         })
         event.preventDefault()
     }
 
+    handleChange(position) {
+        let updatedCheckboxes = this.state.checkboxes
+        updatedCheckboxes[position].checked = !updatedCheckboxes[position].checked
+        console.log("updated", updatedCheckboxes)
+        this.setState({checkboxes: updatedCheckboxes})
+    }
     render() {
         return (
             <div>
-                {this.state.checks.map(check => <CheckControls label={check} />)}
-                <form onSubmit={this.handleSubmit} class="mt-2">
-                    <div class="input-group pe-2 mb-3 align-items-center">
-                        <input 
-                        type="text" 
-                        value={this.state.new_check} 
-                        class="form-control form-for-checkbox" 
-                        onChange={this.handleChange}
-                        placeholder="Other ..."
-                        />
-                        <AiOutlinePlusCircle class="plus-icon"/>
-                    </div>
-                </form>
+                {this.state.checkboxes.map(
+                    ({value, checked}, index) =>
+                    (
+                        <div class="form-check">
+                            <input 
+                            key={"chk-" + value}
+                            class="form-check-input" 
+                            type="checkbox" 
+                            value={value} 
+                            id={"chck-" + value} 
+                            checked={checked}
+                            onChange={() => this.handleChange(index)}
+                             />
+                            <label class="form-check-label" for={"chck-" + value}>
+                                {value}
+                            </label>
+                        </div>
+                    )
+                )}
+                {this.state.dynamic &&
+                    <form onSubmit={this.handleSubmit} class="mt-2">
+                        <div class="input-group pe-2 mb-3 align-items-center">
+                            <input 
+                            type="text" 
+                            value={this.state.new_check} 
+                            class="form-control form-for-checkbox" 
+                            onChange={this.handleType}
+                            placeholder="Other ..."
+                            />
+                            <AiOutlinePlusCircle class="plus-icon"/>
+                        </div>
+                    </form>
+                }
             </div>
             
         )
     }
 }
 
-
 const RadioControls = (props) => {
     return (
         <div class="form-check">
             <input class="form-check-input" type="radio" name={props.name} id={props.label} />
-            <label class="form-check-label" for={props.label}>
-                {props.label}
-            </label>
-        </div>
-    )
-}
-
-const CheckControls = (props) => {
-    return (
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id={props.label} />
             <label class="form-check-label" for={props.label}>
                 {props.label}
             </label>
@@ -116,7 +144,7 @@ export const JobFilter = () => {
     const types = [
         "Internship",
         "Part-time",
-        "Freelance"
+        "Freelance",
     ]
     const domains = [
         "Education",
@@ -149,19 +177,21 @@ export const JobFilter = () => {
                 <Accordion.Item eventKey="1">
                     <Accordion.Header>Job Type</Accordion.Header>
                     <Accordion.Body>
-                    {types.map(type => <CheckControls label={type} />)}   
+                        <CheckControls dynamic={false} values={types} />
+                    {/* {types.map(type => <CheckControls label={type} />)}    */}
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
                     <Accordion.Header>Domain</Accordion.Header>
                     <Accordion.Body>
-                        <DynamicCheckControls checks={domains} />
+                        <CheckControls dynamic={true} values={domains} />
+                        {/* <DynamicCheckControls checks={domains} /> */}
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3">
                     <Accordion.Header>Skills</Accordion.Header>
                     <Accordion.Body>
-                        <DynamicCheckControls checks={skills} />
+                        <CheckControls dynamic={true} values={skills} />
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
