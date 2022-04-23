96f769de00e5c84305a5b989a6b0b50d4feb682f
Rana Abdul Muneem <r.abdulmuneem@gmail.com>
2022-04-23 20:14:10 +0500

    New signup

--- src/components/StudentSignup/StudentSignup.js
+++ src/components/StudentSignup/StudentSignup.js
@@ -6,11 +6,14 @@ import SignUp from "../forms/SignUp";
 import img from "../../images/boy-table.png";
 import bg from "../../images/bg-1.svg";
 
+import NewSignUp from '../forms/NewSignUp'
+
 const StudentSignup = () => {
   return (
     <div className="p-5">
       {/* <SignUpForm2></SignUpForm2> */}
-      <SignUp header="Student Signup" background={bg} image={img} type="student"></SignUp>
+      {/* <SignUp header="Student Signup" background={bg} image={img} type="student"></SignUp> */}
+      <NewSignUp header="Student Signup" background={bg} image={img} type="student" />
     </div>
   );
 };
