e2f7df3cb6f01f3b0876798d54a4fc376a0c17c0
Rana Abdul Muneem <r.abdulmuneem@gmail.com>
2022-05-27 22:53:13 +0500

    Added one click application

--- client/src/components/utils/Buttons.js
+++ client/src/components/utils/Buttons.js
@@ -3,15 +3,13 @@ import './Buttons.css'
 import { useState } from 'react'
 import {AiOutlineStar, AiFillStar} from 'react-icons/ai'
 
-export const SaveButton = () => {
-    const [saved, setSaved] = useState(false)
-
+export const SaveButton = (props) => {
     return (
         <span>
-            {saved ? (
-                <AiFillStar onClick={() => setSaved(!saved)} class="save-button"/>
+            {props.saved ? (
+                <AiFillStar onClick={() => props.setSaved(!props.saved)} class="save-button"/>
             ) : (
-                <AiOutlineStar onClick={() => setSaved(!saved)} class="save-button"/>
+                <AiOutlineStar onClick={() => props.setSaved(!props.saved)} class="save-button"/>
             )}
         </span>
     )
